ac47ce579a567a150cc3df60834076d08deb19e8
Jelmar Gerritsen <jelmargerritsen@gmail.com>
2024-05-28 16:39:05 +0200

    Add start script

--- minecloud_configs/server/start_server.sh
+++ minecloud_configs/server/start_server.sh
@@ -2,5 +2,18 @@
 
 echo "Starting Minecraft server"
 # You can adjust your server start up command here
-/usr/bin/env java -Xmx6144M -Xms1024M -jar server.jar nogui
+/usr/bin/env java \
+  -Xmx6144M \
+  -Xms2048M \
+  -XX:+UseG1GC \
+  -XX:+ParallelRefProcEnabled \
+  -XX:MaxGCPauseMillis=100 \
+  -XX:+UnlockExperimentalVMOptions \
+  -XX:+DisableExplicitGC \
+  -XX:+AlwaysPreTouch \
+  -XX:TargetSurvivorRatio=90 \
+  -XX:G1NewSizePercent=50 \
+  -XX:G1MaxNewSizePercent=80 \
+  -XX:G1MixedGCLiveThresholdPercent=35 \
+  -jar server.jar nogui
 echo "Minecraft server stop"
